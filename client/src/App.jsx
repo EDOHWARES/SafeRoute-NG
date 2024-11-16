47906781e3b80d2896f78980e45a12a127c1fdd0
EDOHWARES <edohemmanuel4real@gmail.com>
2024-11-16 12:28:31 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-600'>React App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,23 @@
+//Modules
+import express from 'express';
+import cors from 'cors';
+
+//Declarations
+const app = express();
+const port = 5000;
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//db connection
+
+//Endpoints
+app.get('/', (req, res)=> {
+    res.send('API is WORKING PERFECTLY...')
+});
+
+//Initialize server
+app.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`);
+});
