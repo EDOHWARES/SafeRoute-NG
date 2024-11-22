4cae0026d01357e806216fc2043008fcfda3e74f
EDOHWARES <edohemmanuel4real@gmail.com>
2024-11-22 06:52:04 +0100

    admin getProfile controller

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import {loginAdmin, getAdminProfile} from '../controllers/adminController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post('/login', loginAdmin);
-adminRouter.get('/profile', getAdminProfile);
+adminRouter.get('/profile', authMiddleware, getAdminProfile);
 
 export default adminRouter;
